e43151b3507673605a95d3ee138ffab712435b2e
Nicola Ferraro <ni.ferraro@gmail.com>
2016-06-28 09:53:48 +0200

    fixed download url

--- setup/install.sh
+++ setup/install.sh
@@ -4,7 +4,7 @@ URL_PREFIX=hbase-
 VERSION=1.0.1
 
 if [ ! -f "hbase-$VERSION-bin.tar.gz" ]; then
-    wget http://archive.eu.apache.org/dist/hbase/${URL_PREFIX}${VERSION}/hbase-${VERSION}-bin.tar.gz
+    wget http://archive.apache.org/dist/hbase/${URL_PREFIX}${VERSION}/hbase-${VERSION}-bin.tar.gz
 fi
 
 tar xzf hbase-${VERSION}-bin.tar.gz -C /usr/lib/
